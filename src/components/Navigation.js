e5c50aa4e0c62e3e975ed135f3a84a56ce29ae24
Nikola Lazarov <nnl160@aubg.edu>
2018-10-10 15:20:24 +0300

    create Navigation component

---
+++ src/components/Navigation.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navigation = () => (<nav class="col-md-7">
+    <ul>
+        <li>
+            <a href="http://nikolalazarov.com" target="__blank">Portfolio</a>
+        </li>
+        <li>
+            <a href="https://github.com/nikelaz?tab=repositories" target="__blank">Repositories</a>
+        </li>
+        <li>
+            <a href="mailto:nnl160@aubg.edu">Contact me</a>
+        </li>
+    </ul>
+</nav>);
+
+export default Navigation;
