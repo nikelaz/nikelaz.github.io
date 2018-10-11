1321100956c872e4202e5c6edc4fc61c4852819b
Nikola Lazarov <nnl160@aubg.edu>
2018-10-12 02:59:08 +0300

    create footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => (
+    <footer>
+        <div class="row">
+            <div class="col">
+                <p>&copy; Nikola Lazarov 2019</p>
+            </div>
+        </div>
+    </footer>
+);
+
+export default Footer;
